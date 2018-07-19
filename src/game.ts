410c67e55d27f7686cce385dd51ec775676c748b
Nathan Douglas <nd9600@gmail.com>
2018-07-19 11:03:01 +0100

    can import ramda!

--- src/game.ts
+++ src/game.ts
@@ -1,5 +1,9 @@
+import * as R from 'ramda';
+
 let gameDiv = document.getElementById("game");
 console.log(gameDiv);
 
-let a = Array(10).fill(" ");
+let a = Array(10);
+let b = R.map(R.always(" "), a);
 console.log(a);
+console.log(b);
