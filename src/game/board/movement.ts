2fdc8992b8176f4aead2bbe7c1aa12950c22711d
Nathan Douglas <nd9600@gmail.com>
2018-08-23 17:09:11 +0100

    renamed

--- src/game/board/movement.ts
+++ src/game/board/movement.ts
@@ -6,31 +6,31 @@ import usefulFunctions from "@/game/usefulFunctions";
 
 /**
  * Returns the position that could be moved in to (if any), given a list of possible positions
- * @param squaresCouldMoveInto Position[] - a list of the possible squares that could be moved in to
+ * @param positionsCouldMoveInto Position[] - a list of the possible squares that could be moved in to
  * @param board Board
  */
-function getPositionToMoveIntoFromPossibleList(squaresCouldMoveInto: Position[], board: Board): Either<IError, Position> {
+function getPositionToMoveIntoFromPossibleList(positionsCouldMoveInto: Position[], board: Board): Either<IError, Position> {
     const wallInWayOfMovementIndex = R.findIndex((possibleWallPosition) => R.equals(
         Place.Wall,
         boardFunctions.getPosition(possibleWallPosition, board)), 
-    squaresCouldMoveInto);
+    positionsCouldMoveInto);
     // const wallInWayOfMovementIndex = R.findIndex(R.equals(
     //     Place.Wall,
     //     R.flip(boardFunctions.getPosition)(board)), 
-    // squaresCouldMoveInto);
+    // positionsCouldMoveInto);
     
-    let squareToMoveInto: Position;
+    let positionToMoveInto: Position;
     if (wallInWayOfMovementIndex === 0) {
         return left(usefulFunctions.makeError("MovementError", "wall immediately above"));
 
     // no wall was found, so we can move to the top of the board
     } else if (wallInWayOfMovementIndex === -1) {
-        squareToMoveInto = squaresCouldMoveInto[squaresCouldMoveInto.length - 1];
+        positionToMoveInto = positionsCouldMoveInto[positionsCouldMoveInto.length - 1];
     } else {
-        squareToMoveInto = squaresCouldMoveInto[wallInWayOfMovementIndex - 1];
+        positionToMoveInto = positionsCouldMoveInto[wallInWayOfMovementIndex - 1];
     }
 
-    return right(squareToMoveInto);
+    return right(positionToMoveInto);
 }
 
 /**
@@ -57,35 +57,35 @@ function getPositionToMoveInto(board: Board, direction: Direction, fromPosition:
                 return left(usefulFunctions.makeError("MovementError", "at bottom of board"));
             }
             const yRange = usefulFunctions.range(fromPosition.y, board.length);
-            const squaresCouldMoveInto = R.map(
+            const positionsCouldMoveInto = R.map(
                 (y): Position => R.assoc("y", y, fromPosition),
                 yRange
             );
 
-            return getPositionToMoveIntoFromPossibleList(squaresCouldMoveInto, board);
+            return getPositionToMoveIntoFromPossibleList(positionsCouldMoveInto, board);
         } case Direction.Left: {
             if (fromPosition.x === 0) {
                 return left(usefulFunctions.makeError("MovementError", "at left of board"));
             }
             const xRange = usefulFunctions.range(fromPosition.x, 0);
-            const squaresCouldMoveInto = R.map(
+            const positionsCouldMoveInto = R.map(
                 (x): Position => R.assoc("x", x, fromPosition),
                 xRange
             );
 
-            return getPositionToMoveIntoFromPossibleList(squaresCouldMoveInto, board);
+            return getPositionToMoveIntoFromPossibleList(positionsCouldMoveInto, board);
         }
         case Direction.Right: {
             if (fromPosition.x === board[0].length - 1) {
                 return left(usefulFunctions.makeError("MovementError", "at right of board"));
             }
             const xRange = usefulFunctions.range(fromPosition.x, board[0].length);
-            const squaresCouldMoveInto = R.map(
+            const positionsCouldMoveInto = R.map(
                 (x): Position => R.assoc("x", x, fromPosition),
                 xRange
             );
 
-            return getPositionToMoveIntoFromPossibleList(squaresCouldMoveInto, board);
+            return getPositionToMoveIntoFromPossibleList(positionsCouldMoveInto, board);
         }
     }
     return usefulFunctions.assertUnreachable(direction);
@@ -95,8 +95,8 @@ const move = (errorHandler: (error: IError) => void, direction: Direction, gameS
     const {characterPosition, board} = gameState;
     boardFunctions.isPositionOnBoard(characterPosition, board);
 
-    const squareToMoveIntoResult = getPositionToMoveInto(board, direction, characterPosition);
-    return squareToMoveIntoResult.fold(
+    const positionToMoveIntoEither = getPositionToMoveInto(board, direction, characterPosition);
+    return positionToMoveIntoEither.fold(
         (error) => {
             errorHandler(error);
             return {characterPosition, board};
