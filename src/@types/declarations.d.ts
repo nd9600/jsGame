5d6ada1486f2ff7659048c1974d74d6936b3ec9d
Nathan Douglas <nd9600@gmail.com>
2018-08-26 16:23:24 +0100

    removed types

--- src/@types/declarations.d.ts
+++
@@ -1,9 +0,0 @@
-declare module "folktale/result" {
-    const Result: {
-        Ok: (parameter: any) => any,
-        Error: (parameter: any) => any,
-        matchWith: (parameter: any) => any,
-    }
-    
-    export default Result;
-}
