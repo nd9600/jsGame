7bb376013c7813a90809a557166ad0927e8b55ae
Nathan Douglas <nd9600@gmail.com>
2018-09-08 15:58:08 +0100

    tested event list creation, added lots of types

--- __tests__/Events/EventListCreationTest.ts
+++ __tests__/Events/EventListCreationTest.ts
@@ -1,6 +1,15 @@
 import EventRunner from "@/core/events/EventRunner";
 import { Command, DispatchedEvent, Direction, Status } from "@/core/myTypes";
 import Event from "@/core/events/Event";
+import InitialSetupEvent from "@/core/events/Game/InitialSetupEvent";
+import InputEvent from "@/core/events/Game/InputEvent";
+import FailedMovementEvent from "@/core/events/Movement/FailedMovementEvent";
+import MovementEvent from "@/core/events/Movement/MovementEvent";
+import SuccessfulMovementEvent from "@/core/events/Movement/SuccessfulMovementEvent";
+import CommandEvent from "@/core/events/Command/CommandEvent";
+import DirectionEvent from "@/core/events/Command/DirectionEvent";
+import StatusChangeEvent from "@/core/events/Command/StatusChangeEvent";
+import PlayerNameChangeEvent from "@/core/events/Command/PlayerNameChangeEvent";
 
 describe("EventApplication", () => {
 
@@ -18,46 +27,19 @@ describe("EventApplication", () => {
             { type: "Event" },
         ];
         const expectedListOfEvents: object[] = [
-            {
-                type: "InitialSetupEvent",
-                types: [ "Event", "InitialSetupEvent" ],
-                data:
-                    { 
-                        initialPlayerName: "x",
-                        size: [Array],
-                        startPoint: [Object],
-                        endPoint: [Object] 
-                    } 
-            },
-            {
-                type: "InputEvent",
-                types: [ "Event", "InputEvent" ],
-                data: "MoveDown" },
-            {
-                type: "FailedMovementEvent",
-                types: [ "Event", "MovementEvent", "FailedMovementEvent" ],
-                data: { name: "error", message: "msg" } },
-            {
-                type: "MovementEvent",
-                types: [ "Event", "MovementEvent" ],
-                data: undefined },
-            {
-                type: "SuccessfulMovementEvent",
-                types: [ "Event", "MovementEvent", "SuccessfulMovementEvent" ],
-                data: { boardID: 0, newCharacterPosition: [Object] } },
-            {
-                type: "CommandEvent",
-                types: [ "Event", "CommandEvent" ],
-                data: "MoveDown" },
-            {
-                type: "DirectionEvent",
-                types: [ "Event", "CommandEvent", "DirectionEvent" ],
-                data: "Down" 
-            }
+            new InitialSetupEvent({initialPlayerName: "x", size: [4, 4], startPoint: { x: 0, y: 0 }, endPoint: { x: 3, y: 3 }}),
+            new InputEvent(Command.MoveDown),
+            new FailedMovementEvent({ name: "error", message: "msg" }),
+            new MovementEvent(),
+            new SuccessfulMovementEvent({boardID: 0, newCharacterPosition: {x: 0, y: 0}}),
+            new CommandEvent(Command.MoveDown),
+            new DirectionEvent(Direction.Down),
+            new PlayerNameChangeEvent({boardID: 0, newPlayerName: "x"}),
+            new StatusChangeEvent({boardID: 0, newStatus: Status.Finished}),
+            new Event(),
         ];
 
         const listOfEvents = EventRunner.makeListOfEvents(listOfEventObjects);
         expect(listOfEvents).toEqual(expectedListOfEvents);
-        console.log(listOfEvents);
     });
 });
