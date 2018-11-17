ca3912814b92f366cb827b2f0ba434b7ba293e21
Nathan Douglas <nd9600@gmail.com>
2018-11-17 01:02:03 +0000

    fixing tests

--- __tests__/Events/EventListCreationTest.ts
+++ __tests__/Events/EventListCreationTest.ts
@@ -12,31 +12,30 @@ import InputEvent from "@/core/events/Game/InputEvent";
 import FailedMovementEvent from "@/core/events/Movement/FailedMovementEvent";
 import MovementEvent from "@/core/events/Movement/MovementEvent";
 import SuccessfulMovementEvent from "@/core/events/Movement/SuccessfulMovementEvent";
+import GameStateFactory from "@/core/factories/GameStateFactory";
 
 describe("EventApplication", () => {
 
     it("applies_a_list_of_events", () => {
         const listOfEventObjects: DispatchedEvent[] = [
             { type: "InitialSetupEvent", data: {initialPlayerName: "x", size: [4, 4], startPoint: { x: 0, y: 0 }, endPoint: { x: 3, y: 3 }} },
-            { type: "InputEvent", data: Command.MoveDown },
+            { type: "InputEvent", data: {command: Command.MoveDown, player: GameStateFactory.defaultPlayer}},
             { type: "FailedMovementEvent", data: {name: "error", message: "msg"} },
             { type: "MovementEvent" },
-            { type: "SuccessfulMovementEvent", data: {boardID: 0, newCharacterPosition: {x: 0, y: 0}} },
-            { type: "CommandEvent", data: Command.MoveDown },
-            { type: "DirectionEvent", data: Direction.Down },
-            { type: "PlayerNameChangeEvent", data: {boardID: 0, newPlayerName: "x"} },
+            { type: "SuccessfulMovementEvent", data: {boardID: 0, playerID: 0, newCharacterPosition: {x: 0, y: 0}} },
+            { type: "DirectionEvent", data: {direction: Direction.Down, player: GameStateFactory.defaultPlayer} },
+            { type: "PlayerNameChangeEvent", data: {playerID: 0, newPlayerName: "x"} },
             { type: "StatusChangeEvent", data: {boardID: 0, newStatus: Status.Finished} },
             { type: "ToggleWallEvent", data: {boardID: 0, positionToToggle: {x: 0, y: 0}} },
         ];
         const expectedListOfEvents: object[] = [
             new InitialSetupEvent({initialPlayerName: "x", size: [4, 4], startPoint: { x: 0, y: 0 }, endPoint: { x: 3, y: 3 }}),
-            new InputEvent(Command.MoveDown),
+            new InputEvent({command: Command.MoveDown, player: GameStateFactory.defaultPlayer}),
             new FailedMovementEvent({ name: "error", message: "msg" }),
             new MovementEvent(),
-            new SuccessfulMovementEvent({boardID: 0, newCharacterPosition: {x: 0, y: 0}}),
-            new CommandEvent(Command.MoveDown),
-            new DirectionEvent(Direction.Down),
-            new PlayerNameChangeEvent({boardID: 0, newPlayerName: "x"}),
+            new SuccessfulMovementEvent({boardID: 0, playerID: 0, newCharacterPosition: {x: 0, y: 0}}),
+            new DirectionEvent({direction: Direction.Down, player: GameStateFactory.defaultPlayer}),
+            new PlayerNameChangeEvent({playerID: 0, newPlayerName: "x"}),
             new StatusChangeEvent({boardID: 0, newStatus: Status.Finished}),
             new ToggleWallEvent({boardID: 0, positionToToggle: {x: 0, y: 0}}),
         ];
