b805fa6401ac53116b03b8ec15ae1d6d79a27322
Nathan Douglas <nd9600@gmail.com>
2018-12-14 20:05:27 +0000

    refactored score tests

--- __tests__/Events/Command/SetPlayerBoardStatusToFinishedEventTest.ts
+++ __tests__/Events/Command/SetPlayerBoardStatusToFinishedEventTest.ts
@@ -6,6 +6,67 @@ import PlayerBoardBuilder from "@/core/player/PlayerBoardBuilder";
 import * as R from "ramda";
 
 describe("SetPlayerBoardStatusToFinishedEvent", () => {
+    
+    function assertScores(
+        p0b0Solved: boolean, 
+        p0b1Solved: boolean, 
+        p1b0Solved: boolean, 
+        p1b1Solved: boolean, 
+        player0Score: number, 
+        player1Score: number
+    ): void {
+        let player0 = GameStateFactory.defaultPlayer();
+        let player1 = GameStateFactory.defaultPlayer();
+        const board0 = GameStateFactory.defaultBoard(player0.id);
+        const board1 = GameStateFactory.defaultBoard(player1.id);
+        
+        let gameState = GameStateFactory.createGameState({players: [player0, player1], boards: [board0, board1]});
+        
+        gameState = gameState
+            .replacePlayerBoard(
+                PlayerBoardBuilder.mergeWithOptions(gameState.getPlayerBoard(player0.id, board0.id), {
+                    characterPosition: p0b0Solved ? board0.endPoint : board0.startPoint
+                })
+            )
+            .replacePlayerBoard(
+                PlayerBoardBuilder.mergeWithOptions(gameState.getPlayerBoard(player0.id, board1.id), {
+                    characterPosition: p0b1Solved ? board1.endPoint : board1.startPoint
+                })
+            )
+            .replacePlayerBoard(
+                PlayerBoardBuilder.mergeWithOptions(gameState.getPlayerBoard(player1.id, board0.id), {
+                    characterPosition: p1b0Solved ? board0.endPoint : board0.startPoint
+                })
+            )
+            .replacePlayerBoard(
+                PlayerBoardBuilder.mergeWithOptions(gameState.getPlayerBoard(player1.id, board1.id), {
+                    characterPosition: p1b1Solved ? board1.endPoint : board1.startPoint
+                })
+            );
+
+        gameState = EventRunner.runEvents([
+            new SetPlayerBoardStatusToFinishedEvent({playerID: player0.id, boardID: board0.id}),
+            new SetPlayerBoardStatusToFinishedEvent({playerID: player0.id, boardID: board1.id}),
+            new SetPlayerBoardStatusToFinishedEvent({playerID: player1.id, boardID: board0.id}),
+            new SetPlayerBoardStatusToFinishedEvent({playerID: player1.id, boardID: board1.id}),
+        ], gameState);
+        
+        //throw new Error(gameState.getCurrentInfo());
+
+        player0 = R.values(gameState.players)[0];
+        player1 = R.values(gameState.players)[1];
+        
+        /*
+         When all PlayerBoards are Finished, if
+            - you complete your maze & opponent doesn't, you get 10 points
+            - both complete a maze, both get 5 points
+            - neither complete maze, 0 points
+            - you don't complete your maze & opponent does, they get 20 points
+        */
+
+        expect(player0.score).toEqual(player0Score);
+        expect(player1.score).toEqual(player1Score);
+    }
 
     it("handles_changing_status", () => {
         const player0 = GameStateFactory.defaultPlayer();
@@ -51,187 +112,20 @@ describe("SetPlayerBoardStatusToFinishedEvent", () => {
     
     //____ ____ ____ ____
     
-    it("player 0 gets 10 points when solving player0 board only", () => {
-        let player0 = GameStateFactory.defaultPlayer();
-        let player1 = GameStateFactory.defaultPlayer();
-        const board0 = GameStateFactory.defaultBoard(player0.id);
-        const board1 = GameStateFactory.defaultBoard(player1.id);
-        
-        let gameState = GameStateFactory.createGameState({players: [player0, player1], boards: [board0, board1]});
-        
-        gameState = gameState
-            .replacePlayerBoard(
-                PlayerBoardBuilder.mergeWithOptions(gameState.getPlayerBoard(player0.id, board0.id), {
-                    characterPosition: board0.endPoint
-                })
-            )
-            .replacePlayerBoard(
-                PlayerBoardBuilder.mergeWithOptions(gameState.getPlayerBoard(player0.id, board1.id), {
-                    characterPosition: board1.startPoint
-                })
-            )
-            .replacePlayerBoard(
-                PlayerBoardBuilder.mergeWithOptions(gameState.getPlayerBoard(player1.id, board0.id), {
-                    characterPosition: board0.startPoint
-                })
-            )
-            .replacePlayerBoard(
-                PlayerBoardBuilder.mergeWithOptions(gameState.getPlayerBoard(player1.id, board1.id), {
-                    characterPosition: board1.startPoint
-                })
-            );
-
-        gameState = EventRunner.runEvents([
-            new SetPlayerBoardStatusToFinishedEvent({playerID: player0.id, boardID: board0.id}),
-            new SetPlayerBoardStatusToFinishedEvent({playerID: player0.id, boardID: board1.id}),
-            new SetPlayerBoardStatusToFinishedEvent({playerID: player1.id, boardID: board0.id}),
-            new SetPlayerBoardStatusToFinishedEvent({playerID: player1.id, boardID: board1.id}),
-        ], gameState);
-
-        player0 = R.values(gameState.players)[0];
-        player1 = R.values(gameState.players)[1];
-        
-        /*
-         When all PlayerBoards are Finished, if
-            - you complete your maze & opponent doesn't, you get 10 points
-            - both complete a maze, both get 5 points
-            - neither complete maze, 0 points
-            - you don't complete your maze & opponent does, they get 20 points
-        */
-
-        expect(player0.score).toEqual(10);
-        expect(player1.score).toEqual(0);
+    it("player 0 gets 10 points when solving player0 board only", () => {            
+        assertScores(true, false, false, false, 10, 0);
     });
 
     it("both players get 5 points when solving player0 board only", () => {
-        let player0 = GameStateFactory.defaultPlayer();
-        let player1 = GameStateFactory.defaultPlayer();
-        const board0 = GameStateFactory.defaultBoard(player0.id);
-        const board1 = GameStateFactory.defaultBoard(player1.id);
-        
-        let gameState = GameStateFactory.createGameState({players: [player0, player1], boards: [board0, board1]});
-        
-        gameState = gameState
-            .replacePlayerBoard(
-                PlayerBoardBuilder.mergeWithOptions(gameState.getPlayerBoard(player0.id, board0.id), {
-                    characterPosition: board0.endPoint
-                })
-            )
-            .replacePlayerBoard(
-                PlayerBoardBuilder.mergeWithOptions(gameState.getPlayerBoard(player0.id, board1.id), {
-                    characterPosition: board1.startPoint
-                })
-            )
-            .replacePlayerBoard(
-                PlayerBoardBuilder.mergeWithOptions(gameState.getPlayerBoard(player1.id, board0.id), {
-                    characterPosition: board0.endPoint
-                })
-            )
-            .replacePlayerBoard(
-                PlayerBoardBuilder.mergeWithOptions(gameState.getPlayerBoard(player1.id, board1.id), {
-                    characterPosition: board1.startPoint
-                })
-            );
-
-        gameState = EventRunner.runEvents([
-            new SetPlayerBoardStatusToFinishedEvent({playerID: player0.id, boardID: board0.id}),
-            new SetPlayerBoardStatusToFinishedEvent({playerID: player0.id, boardID: board1.id}),
-            new SetPlayerBoardStatusToFinishedEvent({playerID: player1.id, boardID: board0.id}),
-            new SetPlayerBoardStatusToFinishedEvent({playerID: player1.id, boardID: board1.id}),
-        ], gameState);
-
-        player0 = R.values(gameState.players)[0];
-        player1 = R.values(gameState.players)[1];
-
-        expect(player0.score).toEqual(5);
-        expect(player1.score).toEqual(5);
+        assertScores(true, false, true, false, 5, 5);
     });
     
+    it("player 0 gets 20 points when solving player 1's maze only", () => {
+        assertScores(false, true, false, false, 20, 0);
+    });
+
     it("both players get 0 points when solving no maze", () => {
-        let player0 = GameStateFactory.defaultPlayer();
-        let player1 = GameStateFactory.defaultPlayer();
-        const board0 = GameStateFactory.defaultBoard(player0.id);
-        const board1 = GameStateFactory.defaultBoard(player1.id);
-        
-        let gameState = GameStateFactory.createGameState({players: [player0, player1], boards: [board0, board1]});
-        
-        gameState = gameState
-            .replacePlayerBoard(
-                PlayerBoardBuilder.mergeWithOptions(gameState.getPlayerBoard(player0.id, board0.id), {
-                    characterPosition: board0.startPoint
-                })
-            )
-            .replacePlayerBoard(
-                PlayerBoardBuilder.mergeWithOptions(gameState.getPlayerBoard(player0.id, board1.id), {
-                    characterPosition: board1.startPoint
-                })
-            )
-            .replacePlayerBoard(
-                PlayerBoardBuilder.mergeWithOptions(gameState.getPlayerBoard(player1.id, board0.id), {
-                    characterPosition: board0.startPoint
-                })
-            )
-            .replacePlayerBoard(
-                PlayerBoardBuilder.mergeWithOptions(gameState.getPlayerBoard(player1.id, board1.id), {
-                    characterPosition: board1.startPoint
-                })
-            );
-
-        gameState = EventRunner.runEvents([
-            new SetPlayerBoardStatusToFinishedEvent({playerID: player0.id, boardID: board0.id}),
-            new SetPlayerBoardStatusToFinishedEvent({playerID: player0.id, boardID: board1.id}),
-            new SetPlayerBoardStatusToFinishedEvent({playerID: player1.id, boardID: board0.id}),
-            new SetPlayerBoardStatusToFinishedEvent({playerID: player1.id, boardID: board1.id}),
-        ], gameState);
-
-        player0 = R.values(gameState.players)[0];
-        player1 = R.values(gameState.players)[1];
-        
-        expect(player0.score).toEqual(0);
-        expect(player1.score).toEqual(0);
+        assertScores(false, false, false, false, 0, 0);
     });
-    
-    it("player 0 get 20 points when solving player 1's maze only", () => {
-        let player0 = GameStateFactory.defaultPlayer();
-        let player1 = GameStateFactory.defaultPlayer();
-        const board0 = GameStateFactory.defaultBoard(player0.id);
-        const board1 = GameStateFactory.defaultBoard(player1.id);
-        
-        let gameState = GameStateFactory.createGameState({players: [player0, player1], boards: [board0, board1]});
-        
-        gameState = gameState
-            .replacePlayerBoard(
-                PlayerBoardBuilder.mergeWithOptions(gameState.getPlayerBoard(player0.id, board0.id), {
-                    characterPosition: board0.startPoint
-                })
-            )
-            .replacePlayerBoard(
-                PlayerBoardBuilder.mergeWithOptions(gameState.getPlayerBoard(player0.id, board1.id), {
-                    characterPosition: board1.endPoint
-                })
-            )
-            .replacePlayerBoard(
-                PlayerBoardBuilder.mergeWithOptions(gameState.getPlayerBoard(player1.id, board0.id), {
-                    characterPosition: board0.startPoint
-                })
-            )
-            .replacePlayerBoard(
-                PlayerBoardBuilder.mergeWithOptions(gameState.getPlayerBoard(player1.id, board1.id), {
-                    characterPosition: board1.startPoint
-                })
-            );
 
-        gameState = EventRunner.runEvents([
-            new SetPlayerBoardStatusToFinishedEvent({playerID: player0.id, boardID: board0.id}),
-            new SetPlayerBoardStatusToFinishedEvent({playerID: player0.id, boardID: board1.id}),
-            new SetPlayerBoardStatusToFinishedEvent({playerID: player1.id, boardID: board0.id}),
-            new SetPlayerBoardStatusToFinishedEvent({playerID: player1.id, boardID: board1.id}),
-        ], gameState);
-
-        player0 = R.values(gameState.players)[0];
-        player1 = R.values(gameState.players)[1];
-
-        expect(player0.score).toEqual(20);
-        expect(player1.score).toEqual(0);
-    });
 });
