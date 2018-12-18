38e25c6a5d457ead03f91d9c97dbdad16df3f2fe
Nathan Douglas <nd9600@gmail.com>
2018-12-18 16:15:17 +0000

    shows basic info

--- src/shell/vue/Game.vue
+++ src/shell/vue/Game.vue
@@ -1,28 +1,76 @@
 <template>
-    <div>hello</div>
+    <div>
+        hello
+
+        <div style="white-space: pre-line;">
+            {{ gameState.getCurrentInfo() }}
+        </div>
+    </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+import * as R from "ramda";
 import InitialSetupEvent from "@/core/events/Game/InitialSetupEvent";
+import GameState from "@/core/GameState";
 import GameStateFactory from "@/core/factories/GameStateFactory";
+import Player from "@/core/player/Player";
+import { InputEventData, Command } from "@/core/@typings/EventDataTypes";
+import InputEvent from "@/core/events/Game/InputEvent";
 import DefaultGameSetup from "@/shell/DefaultGameSetup";
 import EventBus from "@/shell/EventBus";
-import UserInput from "@/shell/interaction/UserInput";
-import * as R from "ramda";
-
-const setup = new DefaultGameSetup();
-const [initialPlayerName, size, startPoint, endPoint, playerIDs, boardIDs] = [setup.getInitialPlayerName(), setup.getSize(), setup.getStartPoint(), setup.getEndPoint(), setup.getPlayerIDs(), setup.getBoardIDs()];
-
-const initialGameSetupData = {initialPlayerName, size, startPoint, endPoint, playerIDs, boardIDs};
-const initialSetupEvent = new InitialSetupEvent(initialGameSetupData);
-const initialGameState = initialSetupEvent.handle(GameStateFactory.createGameState());
-
-const player = R.values(initialGameState.players)[0];
-
-// UserInput.handleUserInput(initialGameState, player);
 
 export default Vue.extend({
-    name: "game"
+    name: "game",
+    data(): {
+        gameState: GameState | null;
+        player: Player | null;
+    } {
+        return {
+            gameState: null,
+            player: null
+        };
+    },
+    watch: {
+
+    },
+    created() {
+        const setup = new DefaultGameSetup();
+        const [initialPlayerName, size, startPoint, endPoint, playerIDs, boardIDs] = [setup.getInitialPlayerName(), setup.getSize(), setup.getStartPoint(), setup.getEndPoint(), setup.getPlayerIDs(), setup.getBoardIDs()];
+
+        const initialGameSetupData = {initialPlayerName, size, startPoint, endPoint, playerIDs, boardIDs};
+        const initialSetupEvent = new InitialSetupEvent(initialGameSetupData);
+        this.gameState = initialSetupEvent.handle(GameStateFactory.createGameState());
+        this.player = R.values(this.gameState.players)[0];
+
+        this.registerCommandListeners();
+    },
+    methods: {
+        registerCommandListeners() {
+            const vm = this;
+            const KEYS_TO_COMMANDS: { [index: string]: Command } = {
+                ArrowUp: Command.MoveUp,
+                ArrowDown: Command.MoveDown,
+                ArrowLeft: Command.MoveLeft,
+                ArrowRight: Command.MoveRight
+            };
+
+            const gameState = vm.gameState!;
+            const player = vm.player!;
+
+            window.addEventListener("keyup", ({code}) => {
+            if (KEYS_TO_COMMANDS.hasOwnProperty(code)) {
+                const inputEventData: InputEventData = {
+                    command: KEYS_TO_COMMANDS[code],
+                    player
+                };
+
+                const inputEvent = new InputEvent(inputEventData);
+                this.gameState = inputEvent.handle(gameState);
+                console.log("gameState: ", gameState);
+            }
+        });
+        }
+    }
 });
 </script>
