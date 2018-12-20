4419747fd28b8706f01893327570e0689d17eef8
Nathan Douglas <nd9600@gmail.com>
2018-12-20 22:34:20 +0000

    trying to make RHS box smaller

--- src/shell/vue/Game.vue
+++ src/shell/vue/Game.vue
@@ -1,10 +1,11 @@
 <template>
     <div>
         <div class="font-serif">My game</div>
-         <div class="flex">
+         <div class="flex flex-shrink m-8">
             <div class="whitespace-pre-line font-sans text-left bg-grey-lighter border border-grey m-4 p-4">
                 {{ gameState.getCurrentInfo() }}
             </div>
+            <div>
             <div class="flex flex-col border border-grey mt-4 ml-4 p-4">
                 <span>Change name</span>
                 <input 
@@ -23,6 +24,7 @@
                 </button>
 
                 <hr class="border border-grey-dark">
+            </div>
             </div>
          </div>
     </div>
