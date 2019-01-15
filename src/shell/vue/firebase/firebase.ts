02ac7639398fb25b512f4596445754bb688642e7
Nathan Douglas <nd9600@gmail.com>
2019-01-15 16:23:37 +0000

    still adding firebase

---
+++ src/shell/vue/firebase/firebase.ts
@@ -0,0 +1,7 @@
+import firebase from "firebase/app";
+import "firebase/database";
+import firebaseConfig from "./firebaseConfig";
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase.database();
